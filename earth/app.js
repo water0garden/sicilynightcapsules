20f554802378a7af43998d33f2e8729872800ff3
water0garden <watergarden4@gmail.com>
2022-11-28 16:03:58 +1300

    earth enters

---
+++ earth/app.js
@@ -0,0 +1,335 @@
+const earth = [
+  {
+    id: 1,
+    title: "Rat race a New York minute",
+    place: "NYC",
+    time: "2.30",
+    img: "./images/item-1.jpeg",
+    desc: `A downpour an apple, under scaffolding, under skyscrapers, under no stars, under side of where I was born. To the core of the Big Apple. First Martini.`,
+  },
+{
+    id: 2,
+    title: "Manhatten elite",
+    place: "NYC",
+    time: "7.30pm",
+    img: "./images/item-2.jpeg",
+    desc: `I asked my friend to teach us how to do Smokey eye, it was less teaching, she did it for me, I sat outside on the fire escape to smoke and saw my BeReal face w smoked out unfamiliar eyes, so I came back into the apartment and wiped it off.`,
+  },
+{
+    id: 3,
+    title: "“Catch my drift inn Central Park”",
+    place: "NYC",
+    time: "4.30pm",
+    img: "./images/item-3.jpeg",
+    desc: `Had some time out to myself in the park of all parks constructed for all, very made up, very dressed up park like a persons natural make up but you can tell its been made up! I had a modela and kept it cool in a running water I found with rocks and some shade, a view of UES ‘houses’ and me. `,
+  },
+{
+    id: 4,
+    title: "En voeYaY to italia",
+    place: "Italy",
+    time: "9.30am",
+    img: "./images/item-4.jpeg",
+    desc: `Monica Vitti: l’troi trilogy, Summer interlude, Bergman. Surviving. I think I prayed in Italy for the first time in my life,.  `,
+  },
+{
+    id: 5,
+    title: "“Peaced and not a hippy”",
+    place: "Italy",
+    time: "6.30pm",
+    img: "./images/item-5.jpeg",
+    desc: `Falling in love for some reason but Kim catral style. My time.  In Italy, will be remembered! I free. Au Naturale.`,
+  },
+{
+    id: 6,
+    title: "“The main course listening to nonas”",
+    place: "Italy",
+    time: "12.20pm",
+    img: "./images/item-6.jpeg",
+    desc: `Nona’s are the pot and the plant to the streets of this town. They hold their stance, they droop in the sun and they create great smells throughout the day`,
+  },
+{
+    id: 7,
+    title: "“History lesson with stoner guide”",
+    place: "Italy",
+    time: "11.14pm",
+    img: "./images/item-7.jpeg",
+    desc: `Castro night drive: A roman Poet: Publio virgilio marone eneide. You can take a boat to Albania and get smokes for cheaper. And there are beautiful fortified citadel overlooking the sea and embellished with Byzantine (basilica) and medieval structures. Strengthened by the mighty castle and some towers. The maintenance of the walls was ensured by the feudal lord until 1806, the year in which feudalism was abolished. Most of the walls were used over the decades as foundations of the perimeter houses of the ancient town; (Bastard houses?) The ancient nucleus of Castro is the only example, in the entire province, of a fortified citadel situated on a rocky hill overlooking the sea. `,
+  },
+{
+    id: 8,
+    title: "Jewels",
+    place: "Puglia",
+    time: "11.48pm",
+    img: "./images/item-8.jpeg",
+    desc: `Who will run this old town in the future`,
+  },
+{
+    id: 9,
+    title: "“Girl is inn a movie”",
+    place: "Puglia",
+    time: "3.23pm",
+    img: "./images/item-9.jpeg",
+    desc: `Making up where to go and making myself up in the bus terminal changing rooms.`,
+  },
+{
+    id: 10,
+    title: "“A painting”",
+    place: "Sicily",
+    time: "7.23am",
+    img: "./images/item-10.jpeg",
+    desc: `Out of context Painting “The Virgin Annunciate is a painting by the Italian Renaissance artist Antonello da Messina, housed in the Palazzo Abatellis, Palermo, region of Sicily, Italy. Probably painted in Sicily in 1476, it shows Mary interrupted at her reading by the Angel of the Annunciation.” White Lotus, Lucia Do you like the Siclia blue waters? `,
+  },
+{
+    id: 11,
+    title: "“Empty but full ovf warm water”",
+    place: "Sicily",
+    time: "3.23pm",
+    img: "./images/item-11.jpeg",
+    desc: `Port, where an old queen bathed. Seats set up for some event.`,
+  },
+{
+    id: 12,
+    title: "“View”",
+    place: "Sicily",
+    time: "6.23am",
+    img: "./images/item-12.jpeg",
+    desc: `If vistas could kill, this is my poison. Its early morning, I go today, so we have last coffee and smoke, this view is the crema to my nero cafe. Theres the wooden seat where I sit, holding my phone up to take this view and the interior, the blue tiles and brown cabinetry and me, ciao'ing .`,
+  },
+{
+    id: 13,
+    title: "“View”",
+    place: "Paris",
+    time: "3.13pm",
+    img: "./images/item-13.jpeg",
+    desc: `I slept 500meters away from the sacre coeur for a week.`,
+  },
+{
+    id: 14,
+    title: "“Traditional baguette”",
+    place: "Paris",
+    time: "2.23pm",
+    img: "./images/item-14.jpeg",
+    desc: `This cat slept on the windowsill of the boulangerie, catted about slowly, seemed very calm but looks nervous in this photo. I liked this grey cats with blue eyes and that’s about it on cats.`,
+  },
+{
+    id: 15,
+    title: "“Muse”",
+    place: "Paris",
+    time: "1.03pm",
+    img: "./images/item-15.jpeg",
+    desc: `Angels arc.`,
+  },
+{
+    id: 16,
+    title: "“Stationary”",
+    place: "Paris",
+    time: "6.13pm",
+    img: "./images/item-16.jpeg",
+    desc: `Here I am.`,
+  },
+{
+    id: 17,
+    title: "“chantal akerman place”",
+    place: "Paris",
+    time: "5.19pm",
+    img: "./images/item-17.jpeg",
+    desc: `After a swim.`,
+  },
+{
+    id: 18,
+    title: "“Charge”",
+    place: "Paris",
+    time: "11.51am",
+    img: "./images/item-18.jpeg",
+    desc: `Weightless.`,
+  },
+{
+    id: 19,
+    title: "Personal shopper",
+    place: "Paris",
+    time: "2.23pm",
+    img: "./images/item-19.jpeg",
+    desc: `It was.`,
+  },
+{
+    id: 20,
+    title: "“Ricard”",
+    place: "Paris",
+    time: "12.13am",
+    img: "./images/item-20.jpeg",
+    desc: `Pastisse`,
+  },
+{
+    id: 21,
+    title: "“Oldest”",
+    place: "Paris",
+    time: "10.23pm",
+    img: "./images/item-21.jpeg",
+    desc: `here`,
+  },
+{
+    id: 22,
+    title: "Shot",
+    place: "Paris",
+    time: "2.23am",
+    img: "./images/item-22.jpeg",
+    desc: `view`,
+  },
+{
+    id: 23,
+    title: "Breakfast leftover lover",
+    place: "Paris",
+    time: "9.00pm",
+    img: "./images/item-23.jpeg",
+    desc: `view`,
+  },
+{
+    id: 24,
+    title: "“Journée”",
+    place: "Paris",
+    time: "12.43am",
+    img: "./images/item-24.jpeg",
+    desc: `…`,
+  },
+{
+    id: 25,
+    title: "“Self portrait”",
+    place: "Paris",
+    time: "2.45pm",
+    img: "./images/item-25.jpeg",
+    desc: `…`,
+  },
+{
+    id: 26,
+    title: "“Journée”",
+    place: "Paris",
+    time: "4.43pm",
+    img: "./images/item-26.jpeg",
+    desc: `…`,
+  },
+{
+    id: 27,
+    title: "“One Shot”",
+    place: "Croatia",
+    time: "9.12am",
+    img: "./images/item-27.jpeg",
+    desc: `…`,
+  },
+{
+    id: 28,
+    title: "“Speed”",
+    place: "Milan",
+    time: "10.44am",
+    img: "./images/item-28.jpeg",
+    desc: `…`,
+  },
+{
+    id: 29,
+    title: "Dream",
+    place: "Florence",
+    time: "11.01am",
+    img: "./images/item-29.jpeg",
+    desc: `…`,
+  },
+{
+    id: 30,
+    title: "“Hair”",
+    place: "Italy",
+    time: "12.00pm",
+    img: "./images/item-30.jpeg",
+    desc: `…`,
+  },
+{
+    id: 31,
+    title: "“first cafe”",
+    place: "Italy",
+    time: "11.10am",
+    img: "./images/item-31.jpeg",
+    desc: `…`,
+  },
+{
+    id: 32,
+    title: "“special”",
+    place: "Greece",
+    time: "6.23am",
+    img: "./images/item-32.jpeg",
+    desc: `…`,
+  },
+];
+
+
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
+
+
+
+// load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayEarthItems(earth);
+  displayEarthButtons();
+});
+
+function displayEarthItems(earthItems) {
+  let displayEarth = earthItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="earth-item">
+      <img src=${item.img} class="photo" alt=${item.title}>
+      <div class="item-info">
+        <header>
+        <h4>${item.title}</h4>
+        <h4 class="time">${item.time}</h4>
+      </header>
+      <p class="item-text">${item.desc}
+      </p>
+      </div>
+    </article>`;
+  });
+  displayEarth = displayEarth.join("");
+  console.log(displayEarth);
+
+  sectionCenter.innerHTML = displayEarth;
+};
+
+
+function displayEarthButtons() {
+  const places = earth.reduce(
+    function (values, item) {
+    if (!values.includes(item.place)) {
+      values.push(item.place);
+    }
+  return values;
+  },
+  ["all"]
+  );
+  const placeBtns = places
+  .map(function (place) {
+    return `<button class="filter-btn" type="button"
+    data-id=${place}>
+      ${place}
+    </button>`
+  })
+  .join("");
+  container.innerHTML = placeBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  // filter buttons
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const place = e.currentTarget.dataset.id;
+      const earthPlace = earth.filter(function (earthItem) {
+        // console.log(earthItem.place);
+      if(earthItem.place === place) {
+        return earthItem;
+        }
+      });
+      // console.log(earthplace);
+      if (place === "all") {
+        displayEarthItems(earth);
+      } else {
+        displayEarthItems(earthPlace);
+      }
+    });
+  });
+
+
+}
